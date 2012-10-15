931a773b20c9dfab1cd800aa20e1329c3890e4f5
Alejandro Murillo Corrales <amurillo@petroperu.com.pe>
2012-10-14 21:14:39 -0500

    tabla cambio funcion

--- lib/stalker.rb
+++ lib/stalker.rb
@@ -92,19 +92,19 @@ class STALKER < Sinatra::Application
 	end
 
 	
-	def ToCell (tag,value)
+	def to_cells (tag,value)
     value.map{ |c| "<#{tag}>#{c}</#{tag}>" }.join   
 	end
 
+	
 	def ToTable (table_array, table_class)
-    headers = "<tr>" + ToCell('th',table_array[0]) + "</tr>"
-    cells = table_array[1..table_array.count].map{ |each_row|
-        "<tr>#{ToCell('td',each_row)}</tr>"             
-    }.join
-
+	headers = "<tr>#{rows[0].keys.to_cells('th')}</tr>"
+	cells = rows.map do |row|
+	"<tr>#{row.values.to_cells('td')}</tr>"
+	end.join("\n  ")
     table = "<table class=\"#{table_class}\"><thead>#{headers}</thead><tbody>#{cells}</tbody></table>"
-end
-
+	end
+	
 	
 end
 
